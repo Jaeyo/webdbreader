361fecf80580d867788cf23a52509e58ffa052eb
leejy <lastiverse@gmail.com>
2015-09-03 09:57:00 +0900

    -

--- src/main/java/com/igloosec/webdbreader/dao/SimpleRepoDAO.java
+++ src/main/java/com/igloosec/webdbreader/dao/SimpleRepoDAO.java
@@ -1,8 +1,8 @@
 package com.igloosec.webdbreader.dao;
 
-import org.json.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.igloosec.webdbreader.common.SingletonInstanceRepo;
 import com.igloosec.webdbreader.rdb.DerbyDataSource;
@@ -11,17 +11,31 @@ public class SimpleRepoDAO {
 	private static final Logger logger = LoggerFactory.getLogger(SimpleRepoDAO.class);
 	private DerbyDataSource ds = SingletonInstanceRepo.getInstance(DerbyDataSource.class);
 	
-	TODO IMME
+	public void insert(String scriptName, String key, String value) {
+		ds.getJdbcTmpl().update("INSERT INTO simple_repo (script_name, simple_repo_key, simple_repo_value) VALUES(?, ?, ?)", scriptName, key, value);
+	} //insert
 	
-	public JSONArray load(){
-		return ds.getJdbcTmpl().queryForJsonArray("SELECT config_key, config_value FROM config");
-	} //load
+	public void update(String scriptName, String key, String value) {
+		ds.getJdbcTmpl().update("UPDATE simple_repo SET simple_repo_value = ? WHERE script_name = ? AND simple_repo_key = ?", value, scriptName, key);
+	} //update
 	
-	public String load(String configKey){
-		return ds.getJdbcTmpl().queryForObject("SELECT config_value FROM config WHERE config_key = ?", new Object[]{ configKey }, String.class);
-	} //load
+	public boolean isExists(String scriptName, String key) {
+		return 1 == ds.getJdbcTmpl().queryForObject("SELECT COUNT(*) "
+				+ "FROM simple_repo WHERE script_name = ? AND simple_repo_key = ?", 
+				new Object[]{ scriptName, key}, Integer.class);
+	} //isExists
 	
-	public void save(String configKey, String configValue){
-		ds.getJdbcTmpl().update("UPDATE config SET config_value = ? WHERE config_key = ?", configValue, configKey);
-	} //save
+	public String select(String scriptName, String key) {
+		try{
+			return ds.getJdbcTmpl().queryForObject("SELECT simple_repo_value FROM simple_repo "
+					+ "WHERE script_name = ? AND simple_repo_key = ?", 
+					new Object[]{scriptName, key}, String.class);
+		} catch(EmptyResultDataAccessException e) {
+			return null;
+		} //catch
+	} //select
+	
+	public void delete(String scriptName) {
+		ds.getJdbcTmpl().update("DELETE FROM simple_repo WHERE script_name = ?", scriptName);
+	} //delete
 } //class
--- src/main/java/com/igloosec/webdbreader/script/bindings/SimpleRepo.java
+++ src/main/java/com/igloosec/webdbreader/script/bindings/SimpleRepo.java
@@ -1,37 +1,47 @@
 package com.igloosec.webdbreader.script.bindings;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.Properties;
-
-import com.igloosec.webdbreader.common.Path;
+import com.igloosec.webdbreader.common.SingletonInstanceRepo;
+import com.igloosec.webdbreader.dao.SimpleRepoDAO;
+import com.igloosec.webdbreader.script.ScriptThread;
 
 public class SimpleRepo {
 	private ScriptLogger logger;
+	private String scriptName;
+	private SimpleRepoDAO simpleRepoDAO = SingletonInstanceRepo.getInstance(SimpleRepoDAO.class);
 
 	public SimpleRepo(ScriptLogger logger) {
 		this.logger = logger;
+		this.scriptName = ScriptThread.currentThread().getScriptName();
 	} // INIT
 
 	public void store(String key, String value) {
+		logger.info(String.format("SimpleRepo.store, key: %s, value: %s", key, value));
 		
-		props.setProperty(key, value);
-		storeProps2File();
+		if(simpleRepoDAO.isExists(this.scriptName, key)) {
+			simpleRepoDAO.update(this.scriptName, key, value);
+		} else {
+			simpleRepoDAO.insert(this.scriptName, key, value);
+		} //if
 	} // store
 
 	public String load(String key) {
-		return props.getProperty(key);
+		logger.info(String.format("SimpleRepo.load, key: %s", key));
+		
+		return simpleRepoDAO.select(this.scriptName, key);
 	} // load
 
 	public String load(String key, String defaultValue) {
-		return props.getProperty(key, defaultValue);
+		logger.info(String.format("SimpleRepo.load, key: %s, defaultValue: %s", key, defaultValue));
+		
+		String value = load(key);
+		if(value == null)
+			return defaultValue;
+		return value;
 	} // load
 
 	public void clear(){
-		props.clear();
-		storeProps2File();
+		logger.info("SimpleRepo.clear");
+		
+		simpleRepoDAO.delete(this.scriptName);
 	} //clear
 } // class
