cb9f28a4289cecc0f49ccbd5ccfd09895f8bb3ed
leejy <lastiverse@gmail.com>
2015-09-08 17:56:45 +0900

    -

--- src/main/java/com/igloosec/webdbreader/Server.java
+++ src/main/java/com/igloosec/webdbreader/Server.java
@@ -29,7 +29,7 @@ public class Server {
 	public static void main(String[] args) throws Exception {
 		registerShutdownHook();
 		
-		TODO test db2db
+//		TODO test db2db
 		
 		new DerbySchemaCreator().check();
 		
